710a43c5ee78ff5a277efcbbc048dccecede8626
JannisK89 <jannis.karanikis@gmail.com>
2024-06-29 09:27:54 +0200

    schema status

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -8,6 +8,7 @@ export const planTable = pgTable('plan_table', {
   text2: text('text2').notNull(),
   text3: text('text3').notNull(),
   createdAt: timestamp('created_at').notNull().defaultNow(),
+  status: text('status').notNull().default('proposed'),
 })
 
 export const occupationTable = pgTable('occupation_table', {
