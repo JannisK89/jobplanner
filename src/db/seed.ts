8a18d09bcf8750e67b14381b723827d22a79668a
JannisK89 <jannis.karanikis@gmail.com>
2024-07-01 19:43:07 +0200

    removed old unused stuff

--- src/db/seed.ts
+++
@@ -1,7 +0,0 @@
-import { db } from '.'
-import { InsertUser, usersTable } from './schema'
-
-export default async function seed(data: InsertUser[]) {
-  await db.delete(usersTable)
-  await db.insert(usersTable).values(data)
-}
