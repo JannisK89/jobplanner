1d977e4914773f2072e9d29f63ee70f6b212e353
JannisK89 <jannis.karanikis@gmail.com>
2024-07-02 19:38:16 +0200

    header jkdev link

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,10 +3,17 @@ import { CgBriefcase } from 'react-icons/cg'
 
 export default function Header() {
   return (
-    <header className="w-full p-3 shadow">
-      <Link className="text-xl ml-20 flex gap-2 items-center" href="/">
+    <header className="w-full p-3 shadow flex justify-between items-center">
+      <Link className="text-xl md:ml-20 flex gap-2 items-center" href="/">
         <CgBriefcase />
         <h1 className="font-bold">Arbetsplaneraren</h1>
+      </Link>
+      <Link
+        className=" text-xs md:text-sm md:mx-16 mx-2 text-sky-900 hover:text-blue-600 transition-colors duration-800 "
+        href="https://www.janniskaranikis.dev/"
+        target="_blank"
+      >
+        JannisKaranikis.Dev
       </Link>
     </header>
   )
